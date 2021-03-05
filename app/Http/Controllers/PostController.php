805080a32dad9967a8b4b88c50fcf76e21eb992c
waddweb <info.webdev@bk.ru>
2021-03-05 18:45:03 +0200

    7th commit. The view Edit.blade.php is created

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -90,7 +90,8 @@ class PostController extends Controller
      */
     public function edit($id)
     {
-        //
+        $post = Post::find($id);
+        return view('posts.edit', compact('post'));
     }
 
     /**
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -2,19 +2,19 @@
 
 @section('content')
 
-    <form action="{{ route('post.store') }}" method="post" enctype="multipart/form-data">
+    <form action="{{ route('post.update', ['id'=>$post->post_id]) }}" method="post" enctype="multipart/form-data">
         @csrf
-        <h3>Create post</h3>
+        <h3>Edit post</h3>
         <div class="form group">
-            <input name="title" type="text" class="form-control" required>
+            <input name="title" type="text" class="form-control" required value="{{ $post->title }}">
         </div>
         <div class="form group">
-            <textarea name="descr" rows="10" class="form-control" required></textarea>
+            <textarea name="descr" rows="10" class="form-control" required>{{ $post->descr }}</textarea>
         </div>
         <div class="form group">
             <input type="file" name="img">
         </div>
-        <input type="submit" value="create post" class="btn btn-outline-success">
+        <input type="submit" value="Save updates" class="btn btn-outline-success">
     </form>
 
 @endsection
