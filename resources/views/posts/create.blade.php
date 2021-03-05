b7c1744eba164fddc58496b37bc67a33b4027ece
waddweb <info.webdev@bk.ru>
2021-03-05 18:58:11 +0200

    8th commit. The file form.blade.php is created. The Views create.blade and edit.blade are updated

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,16 +5,9 @@
     <form action="{{ route('post.store') }}" method="post" enctype="multipart/form-data">
         @csrf
         <h3>Create post</h3>
-        <div class="form group">
-            <input name="title" type="text" class="form-control" required>
-        </div>
-        <div class="form group">
-            <textarea name="descr" rows="10" class="form-control" required></textarea>
-        </div>
-        <div class="form group">
-            <input type="file" name="img">
-        </div>
-        <input type="submit" value="create post" class="btn btn-outline-success">
+        @include('posts.parts.form')
+
+        <input type="submit" value="Create Post" class="btn btn-outline-success">
     </form>
 
 @endsection
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -5,15 +5,8 @@
     <form action="{{ route('post.update', ['id'=>$post->post_id]) }}" method="post" enctype="multipart/form-data">
         @csrf
         <h3>Edit post</h3>
-        <div class="form group">
-            <input name="title" type="text" class="form-control" required value="{{ $post->title }}">
-        </div>
-        <div class="form group">
-            <textarea name="descr" rows="10" class="form-control" required>{{ $post->descr }}</textarea>
-        </div>
-        <div class="form group">
-            <input type="file" name="img">
-        </div>
+        @include('posts.parts.form')
+
         <input type="submit" value="Save updates" class="btn btn-outline-success">
     </form>
 
