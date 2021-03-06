75c04e1c92fe1b4b41616665e32fd6a19123b7c0
waddweb <info.webdev@bk.ru>
2021-03-06 14:00:44 +0200

    the error 404 page is created

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,9 @@
+@extends ('layouts.layout', ['title'=>"404 ERROR"])
+
+@section ('content')
+    <div class="card">
+        <h2>ERROR: 404 / Page not found</h2>
+    </div>
+    <a href="/" class"btn btn-outline-primary">HOME</a>
+
+@endsection
